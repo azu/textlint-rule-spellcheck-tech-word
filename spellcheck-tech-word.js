766cd55635fb1f3e7f1c160c64f7bfee6ff915cd
azu <azuciao@gmail.com>
2015-03-01 11:05:46 +0900

    refactor(npm): curve out spellcheck function

--- spellcheck-tech-word.js
+++ spellcheck-tech-word.js
@@ -1,47 +1,7 @@
 // LICENSE : MIT
 "use strict";
-var dictionaryItems = require("technical-word-rules");
 var RuleHelper = require("textlint-rule-helper").RuleHelper;
-
-/**
- * spell check the text, then return array of result.
- * @param {string} text
- * @returns {SpellCheckResult[]}
- */
-function spellCheckText(text) {
-    var results = [];
-    for (var i = 0, length = dictionaryItems.length; i < length; i++) {
-        var dictionary = dictionaryItems[i];
-        var query = new RegExp(dictionary.pattern, dictionary.flag);
-        var match = query.exec(text);
-        if (!match) {
-            continue;
-        }
-        var matchedString = match[0];
-        // s/Web/Web/iは大文字小文字無視してWebに変換したいという意味に対応する
-        if (dictionary.flag != null) {
-            var strictQuery = new RegExp(dictionary.pattern);
-            var isStrictMatch = strictQuery.test(match[0]);
-            // /Web/i でマッチするけど、 /Web/ でマッチするならそれは除外する
-            if (isStrictMatch) {
-                continue;
-            }
-        }
-        // [start, end]
-        var matchedStartIndex = match.index + 1;
-        var expected = matchedString.replace(query, dictionary.expected);
-        /**
-         * @typedef {{actual: *, expected: (XML|string|void), matchedStartIndex: number}} SpellCheckResult
-         */
-        var result = {
-            actual: matchedString,
-            expected: expected,
-            matchedStartIndex: matchedStartIndex
-        };
-        results.push(result);
-    }
-    return results;
-}
+var spellCheck = require("spellcheck-technical-word").spellCheckText;
 /**
  * @param {RuleContext} context
  */
@@ -54,7 +14,7 @@ module.exports = function (context) {
             return;
         }
         var text = context.getSource(node);
-        var results = spellCheckText(text);
+        var results = spellCheck(text);
 
         results.forEach(function (/*SpellCheckResult*/result) {
             // line, column
